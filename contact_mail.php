6fd1b8f7341f7e6b76f600e70755f533b227f683
Azzeddine LAHLIL <azzeddine.lahlil@gmail.com>
2021-11-29 20:22:09 +0100

    remove unused files

--- contact_mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$toEmail = "azzeddine.lahlil@gmail.com";
-$mailHeaders = "From: " . $_POST["name"] . "<". $_POST["email"] .">\r\n";
-if(mail($toEmail, $_POST["comment"], $mailHeaders)) {
-print "<p class='success'>Contact Mail Sent.</p>";
-} else {
-print "<p class='Error'>Problem in Sending Mail.</p>";
-}
-?>
