9bf126ab9a7d3facc105295707e11771119c42f2
Azzeddine LAHLIL <azzeddine.lahlil@gmail.com>
2021-11-29 22:52:03 +0100

    php mail

--- js/custom.js
+++ js/custom.js
@@ -31,27 +31,56 @@
 		}));
 
         // AJAX CONTACT FORM
-        $(".contactform").on("submit", function() {
-            $(".output_message").text("Loading...");
+        function sendContact() {
+            var valid;	
+            valid = validateContact();
+            if(valid) {
+                jQuery.ajax({
+                    url: "contact_mail.php",
+                    data:'userName='+$("#userName").val()+'&userEmail='+
+                    $("#userEmail").val()+'&subject='+
+                    $("#subject").val()+'&content='+
+                    $(content).val(),
+                    type: "POST",
+                    success:function(data){
+                        $("#mail-status").html(data);
+                    },
+                    error:function (){}
+                });
+            }
+        }
 
-            var form = $(this);
-            $.ajax({
-                url: form.attr("action"),
-                method: form.attr("method"),
-                data: form.serialize(),
-                success: function(result) {
-                    if (result == "success") {
-                        $(".contactform").find(".output_message").addClass("success");
-                        $(".output_message").text("Message Sent!");
-                    } else {
-                        $(".contactform").find(".output_message").addClass("error");
-                        $(".output_message").text("Error Sending!");
-                    }
-                }
-            });
-
-            return false;
-        });
+        function validateContact() {
+            var valid = true;	
+            $(".demoInputBox").css('background-color','');
+            $(".info").html('');
+            if(!$("#userName").val()) {
+                $("#userName-info").html("(required)");
+                $("#userName").css('background-color','#FFFFDF');
+                valid = false;
+            }
+            if(!$("#userEmail").val()) {
+                $("#userEmail-info").html("(required)");
+                $("#userEmail").css('background-color','#FFFFDF');
+                valid = false;
+            }
+            if(!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+                $("#userEmail-info").html("(invalid)");
+                $("#userEmail").css('background-color','#FFFFDF');
+                valid = false;
+            }
+            if(!$("#subject").val()) {
+                $("#subject-info").html("(required)");
+                $("#subject").css('background-color','#FFFFDF');
+                valid = false;
+            }
+            if(!$("#content").val()) {
+                $("#content-info").html("(required)");
+                $("#content").css('background-color','#FFFFDF');
+                valid = false;
+            }
+            return valid;
+        }
 
 		// MATERIAL CAROUSEL
         $(".carousel.carousel-slider").carousel({
